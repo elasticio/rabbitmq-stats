257b1efc52c1acbce1b3f72a18203828a2bfb23e
Pavel Nedelko <pnedelko@gmail.com>
2015-04-30 16:08:20 +0300

    fixes

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-var rabbitStats = require('./lib/rabbitStats')('http://localhost:15672/api/', 'guest', 'guest');
-
-//rabbitStats.getNodes().then(console.log).catch(console.error);
-//rabbitStats.getNode('rabbit@localhost').then(console.log).catch(console.error);
-rabbitStats.getOverview().then(console.log).catch(console.error);
+module.exports = require('./lib/rabbitStats');
