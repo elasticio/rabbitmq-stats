f722ec89bafd8f9002c82737d2d93da6b39969eb
ellenaua <elenasharovar@gmail.com>
2015-09-30 19:08:47 +0300

    Added basic tests

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var jasmine = require('gulp-jasmine');
+var jscs = require('gulp-jscs');
+
+var paths = {
+    code: ['./lib/**/*.js'],
+    spec: ['./spec/**/*.spec.js'],
+    coverageReport: 'coverage/lcov.info'
+};
+
+gulp.task('jasmine', function() {
+    gulp
+        .src(paths.spec)
+        .pipe(jasmine({
+            includeStackTrace: true,
+            verbose: true
+        }));
+});
+
+gulp.task('jscs', function() {
+    return gulp
+        .src(paths.code)
+        .pipe(jscs());
+});
+
+gulp.task('test', ['jasmine']);
+gulp.task('default', ['test']);
---
+++ spec/rabbitStats.spec.js
@@ -0,0 +1,90 @@
+describe('RabbitStats test', function () {
+
+    var RabbitStats = require('../lib/rabbitStats.js');
+    var nock = require('nock');
+
+    var instance = RabbitStats(
+        'http://some-host.com',
+        'guest',
+        'pass'
+    );
+
+    describe('client', function () {
+
+        it('all necessry methods should be present', function (done) {
+            expect(instance.getVhostQueues).toBeDefined();
+            expect(instance.putUser).toBeDefined();
+            expect(instance.setUserPermissions).toBeDefined();
+            done();
+        });
+
+    });
+
+    describe('getVhostQueues', function () {
+        it('successful request', function (done) {
+
+            nock('http://some-host.com:80')
+                .get('/api/queues/%2F')
+                .reply(200, {"data": {"queue1": "params"}});
+
+            instance.getVhostQueues('%2F')
+                .then(checkResponse);
+
+            function checkResponse(data){
+                expect(data).toEqual({"data": {"queue1": "params"}});
+                done();
+            }
+        });
+
+        it('failed request', function (done) {
+
+            nock('http://some-host.com:80')
+                .get('/api/queues/%2F')
+                .reply(404, "Request failed");
+
+            instance.getVhostQueues('%2F')
+                .catch(checkError);
+
+            function checkError(err){
+                expect(err.message).toEqual('404 - Request failed');
+                done();
+            }
+        });
+
+    });
+
+    describe('putUser', function () {
+        it('successful request', function (done) {
+
+            nock('http://some-host.com:80')
+                .put('/api/users/testuser')
+                .reply(200);
+
+            instance.putUser('testuser', {password: "123456"})
+                .then(checkResponse);
+
+            function checkResponse(data){
+                expect(data).toEqual(undefined);
+                done();
+            }
+        });
+    });
+
+    describe('setUserPermissions', function () {
+        it('successful request', function (done) {
+
+            nock('http://some-host.com:80')
+                .put('/api/permissions/%2F/testuser')
+                .reply(200);
+
+            instance.setUserPermissions('testuser', '%2F', {permission1: "value1", permission2: "value2"})
+                .then(checkResponse);
+
+            function checkResponse(data){
+                expect(data).toEqual(undefined);
+                done();
+            }
+        });
+    });
+
+});
