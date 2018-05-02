57c7c436efa218c95bdc4eae1118e8d8c3f2471f
Eugene Obrezkov <ghaiklor@gmail.com>
2018-05-02 17:00:40 +0300

    Update deps and add Content-Type

--- spec/rabbitStats.spec.js
+++ spec/rabbitStats.spec.js
@@ -1,78 +1,64 @@
+const RabbitStats = require('../lib/rabbitStats.js');
+const nock = require('nock');
+
+const instance = RabbitStats('http://some-host.com', 'guest', 'pass');
+const badHeadersForDelete = {
+    badheaders: {
+        accept: 'application/json'
+    }
+};
+
 describe('RabbitStats test', function () {
-
-    var RabbitStats = require('../lib/rabbitStats.js');
-    var nock = require('nock');
-
-    var instance = RabbitStats(
-        'http://some-host.com',
-        'guest',
-        'pass'
-    );
-
-    var badHeadersForDelete = {
-        badheaders: {
-            accept: 'application/json'
-        }
-    };
-
-
-    describe('client', function() {
-
-        it('all necessry methods should be present', function(done) {
+    describe('client', function () {
+        it('all necessry methods should be present', function (done) {
             expect(instance.getVhostQueues).toBeDefined();
             expect(instance.putUser).toBeDefined();
             expect(instance.setUserPermissions).toBeDefined();
             done();
         });
-
     });
 
     describe('getVhostQueues', function () {
         it('successful request', function (done) {
-
             nock('http://some-host.com:80')
                 .get('/api/queues/%2F')
-                .reply(200, {"data": {"queue1": "params"}});
+                .reply(200, { "data": { "queue1": "params" } });
 
             instance.getVhostQueues('/')
                 .then(checkResponse)
-                .catch(done.fail);;
+                .catch(done.fail);
 
-            function checkResponse(data){
-                expect(data).toEqual({"data": {"queue1": "params"}});
+            function checkResponse(data) {
+                expect(data).toEqual({ "data": { "queue1": "params" } });
                 done();
             }
         });
 
         it('failed request', function (done) {
-
             nock('http://some-host.com:80')
                 .get('/api/queues/%2F')
                 .reply(404, "Request failed");
 
-            instance.getVhostQueues('/')
-                .catch(checkError);
+            instance.getVhostQueues('/').catch(checkError);
 
-            function checkError(err){
-                expect(err.message).toEqual('404 - Request failed');
+            function checkError(err) {
+                expect(err.message).toEqual('404 - "Request failed"');
                 done();
             }
         });
-
     });
 
     describe('putUser', function () {
         it('successful request', function (done) {
-
             nock('http://some-host.com:80')
                 .put('/api/users/testuser')
                 .reply(200);
 
-            instance.putUser('testuser', {password: "123456"})
+            instance.putUser('testuser', { password: "123456" })
                 .then(checkResponse)
                 .catch(done.fail);
 
-            function checkResponse(data){
+            function checkResponse(data) {
                 expect(data).toEqual(undefined);
                 done();
             }
@@ -81,24 +67,23 @@ describe('RabbitStats test', function () {
 
     describe('setUserPermissions', function () {
         it('successful request', function (done) {
-
             nock('http://some-host.com:80')
                 .put('/api/permissions/%2F/testuser')
                 .reply(200);
 
-            instance.setUserPermissions('testuser', '/', {permission1: "value1", permission2: "value2"})
+            instance.setUserPermissions('testuser', '/', { permission1: "value1", permission2: "value2" })
                 .then(checkResponse)
                 .catch(done.fail);
 
-            function checkResponse(data){
+            function checkResponse(data) {
                 expect(data).toEqual(undefined);
                 done();
             }
         });
     });
 
-    describe('deleteVhostQueueContents', function() {
-        it('successful request', function(done) {
+    describe('deleteVhostQueueContents', function () {
+        it('successful request', function (done) {
             nock('http://some-host.com:80', badHeadersForDelete)
                 .delete('/api/queues/super-host/important-queue/contents')
                 .reply(204);
@@ -113,21 +98,17 @@ describe('RabbitStats test', function () {
             }
         });
 
-        it('failed request', function(done) {
-
+        it('failed request', function (done) {
             nock('http://some-host.com:80', badHeadersForDelete)
                 .delete('/api/queues/super-host/important-queue/contents')
                 .reply(500, "Error");
 
-            instance.deleteVhostQueueContents('super-host', 'important-queue')
-                .catch(checkError);
+            instance.deleteVhostQueueContents('super-host', 'important-queue').catch(checkError);
 
             function checkError(err) {
-                expect(err.message).toEqual('500 - Error');
+                expect(err.message).toEqual('500 - "Error"');
                 done();
             }
         });
-
     });
-
 });
